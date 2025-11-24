872a0fcbdc8fe4ccd3a2cdb9f89e8defd87a251d
Tonny Bett <tonny.bett1@student.moringaschool.com>
2025-11-24 22:12:58 +0300

    Completed instance method lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
+# class Dog:
+#     # Class body goes here
 
-    #Instance method definition
-    pass
+#     #Instance method definition
+#     pass
+class Dog:
+    # Instance method bark
+    def bark(self):
+        print("Woof!")
+
+    # Instance method sit
+    def sit(self):
+        print("The dog is sitting.")
